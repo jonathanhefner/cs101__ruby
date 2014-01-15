3e2063451313335afafd88dc8a78ef296241e20c
Jonathan Hefner <jonathan.hefner@gmail.com>
2014-01-15 12:05:30 -0500

    Add (and organize) more MergeSort tests

--- merge_sort_spec.rb
+++ merge_sort_spec.rb
@@ -23,24 +23,32 @@ describe MergeSort do
     it 'merges right-heavy inputs' do
       MergeSort.merge([1, 3], [2, 4, 5]).should == [1, 2, 3, 4, 5]
     end
+    
+    it 'merges duplicate elements' do
+      MergeSort.merge([1, 3, 5], [2, 3, 4]).should == [1, 2, 3, 3, 4, 5]
+    end
   end
 
 
   describe '#sort' do
-    it 'sorts an empty list' do
-      MergeSort.sort([]).should == []
-    end
-    
-    it 'sorts a 1-element list' do
-      MergeSort.sort([1]).should == [1]
-    end
-    
     it 'sorts an even number of elements' do
       MergeSort.sort([4, 3, 2, 1]).should == [1, 2, 3, 4]
     end
     
     it 'sorts an odd number of elements' do
       MergeSort.sort([5, 4, 3, 2, 1]).should == [1, 2, 3, 4, 5]
+    end
+    
+    it 'sorts duplicate elements' do
+      MergeSort.sort([4, 3, 1, 3, 2]).should == [1, 2, 3, 3, 4]
+    end
+    
+    it 'works on an empty list' do
+      MergeSort.sort([]).should == []
+    end
+    
+    it 'works on a 1-element list' do
+      MergeSort.sort([1]).should == [1]
     end
     
     it 'works on an already sorted array' do

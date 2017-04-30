3a61ad1f7fb7ae0a5614dbf1dcae780416e7a0d4
Jonathan Hefner <jonathan.hefner@gmail.com>
2017-04-30 13:18:41 -0500

    Fix bug in AvlTree#delete
    Forgetting to properly set the updated child node caused duplicate
    values when that child was a leaf (with no children of its own).

--- lib/avl_tree.rb
+++ lib/avl_tree.rb
@@ -3,27 +3,27 @@ class AvlTree < Struct.new(:val, :left, :right)
   def initialize(v)
     self.val = v
   end
-  
+
   def height
     [left_height, right_height].max
   end
-  
+
   def left_height
     left ? (left.height + 1) : 0
   end
-  
+
   def right_height
     right ? (right.height + 1) : 0
   end
-  
+
   def balance_factor
     right_height - left_height
   end
-  
+
   def min
     left ? left.min : val
   end
-  
+
   def max
     right ? right.max : val
   end
@@ -35,25 +35,25 @@ class AvlTree < Struct.new(:val, :left, :right)
     self[lr] = self[lr] ? self[lr].insert(new_val) : AvlTree.new(new_val)
     rebalance
   end
-  
+
   # NOTE returns the new root/node to replace the current one
   def delete(del_val)
     if val == del_val # delete this node
       if left && right
-        self.val = left.max    # ALT: self.val = right.min
-        left.delete(self.val)  # ALT: right.delete(self.val)
+        self.val = left.max
+        self.left = left.delete(left.max)
         rebalance
       else
         left || right
       end
-      
+
     else
       lr = del_val < val ? :left : :right
       self[lr] = self[lr].delete(del_val) if self[lr]
       rebalance
     end
   end
-  
+
   # NOTE returns the new root/node to replace the current one
   def rebalance
     if balance_factor < -1 # Left-Left or Left-Right
@@ -61,18 +61,18 @@ class AvlTree < Struct.new(:val, :left, :right)
         self.left = left.rotate_left
       end
       rotate_right
-      
+
     elsif balance_factor > 1 # Right-Right or Right-Left
       if right.balance_factor < 0 # Right-Left
         self.right = right.rotate_right
       end
       rotate_left
-      
+
     else
       self
     end
   end
-  
+
   # NOTE returns the new root/node to replace the current one
   def rotate_left
     n = self.right
@@ -80,7 +80,7 @@ class AvlTree < Struct.new(:val, :left, :right)
     n.left = self
     n
   end
-  
+
   # NOTE insert returns the new root/node to replace the current one
   def rotate_right
     n = self.left
@@ -88,40 +88,40 @@ class AvlTree < Struct.new(:val, :left, :right)
     n.right = self
     n
   end
-  
+
   def pre_order(acc=[])
     acc << val
     left.pre_order(acc) if left
     right.pre_order(acc) if right
     acc
   end
-    
+
   def in_order(acc=[])
     left.in_order(acc) if left
     acc << val
     right.in_order(acc) if right
     acc
   end
-  
+
   def post_order(acc=[])
     left.post_order(acc) if left
     right.post_order(acc) if right
     acc << val
     acc
   end
-  
+
   def breadth_first
     acc = []
     queue = [self]
-    
+
     while queue.any?
       n = queue.shift
       acc << n.val
       queue.push(n.left) if n.left
       queue.push(n.right) if n.right
     end
-    
+
     acc
   end
-  
+
 end
